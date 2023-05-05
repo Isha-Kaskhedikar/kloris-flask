04cf60eb034fb8b41a365e1a7cdac972cecf6f05
AparnaJadhav-01 <aparna.jadhav@somaiya.edu>
2023-05-05 16:39:25 +0530

    merge conflict resolved

--- run.py
+++ run.py
@@ -2,3 +2,6 @@ from application import app
 
 if __name__ == "__main__":
     app.run(debug=True,host='192.168.0.101', port=8000)
+
+if __name__ == "__main__":
+    app.run(debug=True,host='192.168.0.101', port=8000)
